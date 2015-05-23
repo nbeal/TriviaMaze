1dec30f98cbf95d4088061340701c63a7aa4277e
Nathan Beal <bealnj@gmail.com>
2015-05-22 21:11:59 -0700

    Added Windows Form and Project Files

---
+++ GUI Form/ConsoleApplication4/Game.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApplication4
+{
+    public partial class TriviaMaze : Form
+    {
+        public TriviaMaze()
+        {
+            InitializeComponent();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartScreen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartScreen.Visible = false;
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutUs us = new AboutUs();
+            us.Show();
+        }
+    }
+}
---
+++ GUI Form/ConsoleApplication4/Room.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    public class Room
+    {
+        private int ID;
+        private int north, west, south, east;
+        private int nclear, wclear, sclear, eclear;
+        private Boolean win = false;
+
+        public int getID()
+        {
+            return this.ID;
+        }
+
+        public void setID(String passed)
+        {
+            this.ID = Convert.ToInt32(passed);
+        }
+
+        public int getExit(String userInput)
+        {
+        switch(userInput)
+		{
+			case "north":
+				//return getNorth();
+			case "west":
+				//return getWest();
+			case "south":
+				//return getSouth();
+			case "east":
+				//return getEast();
+			default:
+				Console.WriteLine("Invalid Command");
+			break;
+		}
+		return -1;
+    }
+    
+
+        public void setExits(String passed)
+        {
+            //north west south east
+            String[] parsed = passed.Split('-');
+
+            north = Convert.ToInt32(passed[0]);
+            west = Convert.ToInt32(passed[1]);
+            south = Convert.ToInt32(passed[2]);
+            east = Convert.ToInt32(passed[3]);
+        }
+
+        public void setWin(Boolean passed)
+        {
+            this.win = passed;
+        }
+
+        public Boolean getWin()
+        {
+            return win;
+        }
+
+    }
+}
