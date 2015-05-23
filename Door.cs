4140939e75e1f657f96eb7e3a5d0565ed0143753
nateowens <nathanowens11@gmail.com>
2015-05-22 22:02:27 -0700

    Created door object
    Created a door object that holds questions from database

---
+++ Door.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Door
+{
+    public string Question { get; set; }
+    public string Answer { get; set; }
+    public bool Unlocked { get; set; }
+
+	public Door(string question, string answer, bool unlocked)
+	{
+        Question = question;
+        Answer = answer;
+        Unlocked = unlocked;
+	}
+}
