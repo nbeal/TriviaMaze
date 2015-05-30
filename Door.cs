21484200f5300b932f2519ba6f85daee18375eca
nateowens <nathanowens11@gmail.com>
2015-05-29 23:23:25 -0700

    Implemented database into door object

--- Door.cs
+++ Door.cs
@@ -1,5 +1,44 @@
 ﻿using System;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
 
+namespace TriviaMaze
+{
+      class DataClass
+      {
+        private SQLiteConnection sqlite;
+
+        public DataClass()
+        {
+              //This part killed me in the beginning.  I was specifying "DataSource"
+              //instead of "Data Source"
+              sqlite = new SQLiteConnection("Data Source=/bin/Debug/MazeQA2.db");
+
+        }
+
+        public DataTable selectQuery(string query)
+        {
+              SQLiteDataAdapter ad;
+              DataTable dt = new DataTable();
+
+              try
+              {
+                    SQLiteCommand cmd;
+                    sqlite.Open();  //Initiate connection to the db
+                    cmd = sqlite.CreateCommand();
+                    cmd.CommandText = query;  //set the passed query
+                    ad = new SQLiteDataAdapter(cmd);
+                    ad.Fill(dt); //fill the datasource
+              }
+              catch(SQLiteException ex)
+              {
+                    //Add your exception code here.
+              }
+              sqlite.Close();
+              return dt;
+  }
+}
 public class Door
 {
     public string Question { get; set; }
